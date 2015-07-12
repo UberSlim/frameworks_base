a16488acbb171ba9c2e3f6f08ec8be0571d10571
SohamJ <soham4jam@gmail.com>
2015-07-11 21:30:12 -0400

    Fix derps
    Signed-off-by: ROM-Jeremy <Disciple97414@Gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/Keyguard/src/com/android/keyguard/KeyguardStatusView.java
    
    Change-Id: I2a71d1f64fec56fff3f7efbbda9b9d88e4433f5b

--- packages/Keyguard/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardStatusView.java
@@ -38,6 +38,7 @@ import android.widget.GridLayout;
 import android.widget.TextClock;
 import android.widget.TextView;
 
+import com.android.internal.util.slim.ImageHelper;
 import com.android.internal.widget.LockPatternUtils;
 
 import java.util.Locale;
